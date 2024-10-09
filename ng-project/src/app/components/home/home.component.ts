c86905eaf35b0f53ca25d31d8cb916a8ef528b7e
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-09 23:19:48 +0200

    almost finished the exercise

--- ng-project/src/app/components/home/home.component.ts
+++ ng-project/src/app/components/home/home.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { PostInterface } from '../../../interfaces/post.interface';
+import { Component, OnInit } from '@angular/core';
+import { iPost } from '../../interfaces/i-post';
+import { iJSONresponse } from '../../interfaces/i-jsonresponse';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  featuredPost!:iPost
+  postArr:iPost[] = []
+
+  ngOnInit(): void {
+
+    fetch ('db.json')
+    .then (res => <Promise<iJSONresponse>> res.json())
+    .then (res => {
+
+      this.featuredPost = res.posts[0]
+      this.postArr = res.posts
+    })
+
+  }
 
 }
